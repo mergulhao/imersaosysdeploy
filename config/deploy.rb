a2886a5862f1dce7c7c6b88f849484b2915ee448
Sylvestre Mergulhao <contato@mergulhao.info>
2010-04-16 00:00:59 -0300

    capify

---
+++ config/deploy.rb
@@ -0,0 +1,17 @@
+set :application, "vidaloucavida.com.br"
+set :repository,  "git://github.com/mergulhao/imersaosysdeploy.git"
+
+set :user, "railsapps"
+set :use_sudo, false
+set :deploy_to, "/home/#{user}/#{application}"
+set :scm, :git
+
+server application, :app, :web, :db, :primary => true
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
