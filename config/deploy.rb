3153e84921284b44331350cac07aa98e751f31df
Sylvestre Mergulhao <contato@mergulhao.info>
2010-04-24 09:50:46 -0300

    update deploy to use custom database.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,10 +10,17 @@ set :ssh_options, {:keys => File.join(ENV["HOME"], "private-key")}
 
 server application, :app, :web, :db, :primary => true
 
+after "deploy:update_code", "deploy:custom_symlinks"
+
 namespace :deploy do
   task :start do ; end
   task :stop do ; end
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+
+  task :custom_symlinks do
+    run "rm -rf #{release_path}/config/database.yml"
+    run "ln -s #{shared_path}/database.yml #{release_path}/config/database.yml"
   end
 end
